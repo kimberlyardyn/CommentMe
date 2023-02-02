ee01c5f98f27c8ba0d3e88aeb82f1154a42fb6ac
Kimberly Wright <kimberlywright@Kimberlys-MacBook-Pro.local>
2023-02-01 22:00:22 -0800

    removed unnecessary functions and html, improved prompts and aesthetics, basic structure is fine.

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -11,11 +11,12 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
-    document.getElementById("comment").onclick = comment;
-    document.getElementById("selectcomment").onclick = selectaicomment;
+    // document.getElementById("run").onclick = run;
+    // document.getElementById("comment").onclick = comment;
+    // document.getElementById("selectcomment").onclick = selectaicomment;
     document.getElementById("grammar").onclick = correctgrammar;
     document.getElementById("mySelect").onchange = givenprompt;
+    document.getElementById("mySelect2").onchange = quickcomment;
     document.getElementById("sendCreatedPrompt").onsubmit = createprompt;
     document.getElementById("myInput").onchange = createprompt;
   }
@@ -41,59 +42,60 @@ async function generateText(message) {
     model: "text-davinci-002",
     prompt: message,
     temperature: 0.7,
-    max_tokens: 70,
+    max_tokens: 120,
   });
   return completion.data.choices[0].text;
 }
 
-export async function run() {
-  return Word.run(async (context) => {
-    /**
-     * Insert your Word code here
-     */
+// export async function run() {
+//   return Word.run(async (context) => {
+//     /**
+//      * Insert your Word code here
+//      */
 
-    // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+//     // insert a paragraph at the end of the document.
+//     const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
 
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
+//     // change the paragraph color to blue.
+//     paragraph.font.color = "blue";
 
-    await context.sync();
-  });
-}
+//     await context.sync();
+//   });
+// }
 
-export async function comment() {
-  // Set a comment on the selected content.
-  return Word.run(async (context) => {
-    const text = "abcd";
-    const comment = context.document.getSelection("Hello World").insertComment(text);
+// export async function comment() {
+//   // Set a comment on the selected content.
+//   return Word.run(async (context) => {
+//     const text = "abcd";
+//     const comment = context.document.getSelection("Hello World").insertComment(text);
 
-    // Load object for display in Script Lab console.
-    comment.load();
-    await context.sync();
-  });
-}
+//     // Load object for display in Script Lab console.
+//     comment.load();
+//     await context.sync();
+//   });
+// }
 
-export async function selectaicomment() {
-  return Word.run(async (context) => {
-    // Get the current selection from the document
-    Office.context.document.getSelectedDataAsync(Office.CoercionType.Text, function (asyncResult) {
-      if (asyncResult.status == Office.AsyncResultStatus.Failed) {
-        write("Action failed. Error: " + asyncResult.error.message);
-      } else {
-        write(asyncResult.value);
-      }
-    });
+// export async function selectaicomment() {
+//   return Word.run(async (context) => {
+//     // Get the current selection from the document
+//     Office.context.document.getSelectedDataAsync(Office.CoercionType.Text, function (asyncResult) {
+//       if (asyncResult.status == Office.AsyncResultStatus.Failed) {
+//         write("Action failed. Error: " + asyncResult.error.message);
+//       } else {
+//         write(asyncResult.value);
+//       }
+//     });
 
-    // Function that writes to a div with id='message' on the page.
-    async function write(message) {
-      const aitext = await generateText(message);
-      const comment = context.document.getSelection("Hello World").insertComment(aitext);
-      comment.load();
-      await context.sync();
-    }
-  });
-}
+//     // Function that writes to a div with id='message' on the page.
+//     async function write(message) {
+//       const aitext = await generateText(message);
+//       const comment = context.document.getSelection(context).insertComment(message + aitext);
+//       comment.load();
+//       await context.sync();
+//       document.getElementById("mySelect").selectedIndex = 0;
+//     }
+//   });
+// }
 
 // COULD USE WORK
 //Find grammar errors in text, and then leave comments explaining solutions and why they are errors
@@ -114,23 +116,43 @@ export async function correctgrammar() {
     // Function that writes to a div with id='message' on the page.
     async function write2(error_message) {
       const message =
-        "Explain all the grammatical errors in this sentence and provide an example of the sentence grammatically corrected:" +
+        "Explain the grammatical errors in this sentence and provide an example of the sentence grammatically corrected:" +
         error_message;
       const aitext = await generateText(message);
-      const comment = context.document.getSelection("Hello World").insertComment(aitext);
+      const comment = context.document.getSelection(context).insertComment(aitext);
       comment.load();
       await context.sync();
     }
   });
 }
 
-//for the prompt creation
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-
-//NEEDS WORK
-// Button Options: Write/Paste own prompt into text box
 export async function givenprompt() {
+  // Set a comment on the selected content, AI produces text response in comment
+  return Word.run(async (context) => {
+    // Get the current selection from the document
+    Office.context.document.getSelectedDataAsync(Office.CoercionType.Text, function (asyncResult) {
+      if (asyncResult.status == Office.AsyncResultStatus.Failed) {
+        write4("Action failed. Error: " + asyncResult.error.message);
+      } else {
+        const selected_text = asyncResult.value;
+        write4(selected_text);
+      }
+    });
+
+    // Function that writes to a div with id='message' on the page.
+    async function write4(selected_text) {
+      const prompt = document.getElementById("mySelect");
+      const aiprompt = prompt.options[prompt.selectedIndex].innerHTML;
+      const aitext = await generateText(aiprompt + selected_text);
+      const comment = context.document.getSelection(selected_text).insertComment(aiprompt + aitext);
+      comment.load();
+      await context.sync();
+      document.getElementById("mySelect").selectedIndex = 0;
+    }
+  });
+}
+
+export async function quickcomment() {
   // Set a comment on the selected content, produces selected_text
   return Word.run(async (context) => {
     // Get the current selection from the document
@@ -145,11 +167,12 @@ export async function givenprompt() {
 
     // Function that writes to a div with id='message' on the page.
     async function write4(selected_text) {
-      const prompt = document.getElementById("mySelect").value;
-      const aitext = await generateText(prompt + selected_text);
-      const comment = context.document.getSelection(selected_text).insertComment(aitext);
+      const prompt = document.getElementById("mySelect2");
+      var text = prompt.options[prompt.selectedIndex].innerHTML;
+      const comment = context.document.getSelection(selected_text).insertComment(text);
       comment.load();
       await context.sync();
+      document.getElementById("mySelect2").selectedIndex = 0;
     }
   });
 }
